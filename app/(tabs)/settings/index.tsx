b540d9f2434419d3823176571720864d4929ed38
Vishavjeet Singh <vishavjeetgill133@gmail.com>
2026-04-15 11:54:28 -0600

    feat: Settings index and profile

--- app/(tabs)/settings/index.tsx
+++ app/(tabs)/settings/index.tsx
@@ -1,12 +1,114 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import FormField from "@/components/common/FormField";
+import SafeHeader from "@/components/common/SafeHeader";
+import ColorPlate from "@/components/settings/ColorPlate";
+import ProfileCard from "@/components/settings/ProfileCard";
+import SwitchCard from "@/components/settings/SwitchCard";
+import { useTheme } from "@/context/ThemeContext";
+import { useUser } from "@/context/UserContext";
+import { THEME_COLOR } from "@/lib/constant";
+import { theme } from "@/styles/theme";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { router } from "expo-router";
+import React, { useState } from "react";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 const Index = () => {
+  const [isReminderOn, setIsReminderOn] = useState(false);
+  const { themeName, changeTheme, activeColors } = useTheme();
+  const { profileName } = useUser();
+
+  const handleProfile = () => {
+    router.push({ pathname: "/settings/profile" });
+  };
   return (
-    <View>
-      <Text>settings index page</Text>
+    <View style={styles.container}>
+      <SafeHeader style={styles.header} title="Settings" />
+      {profileName !== "" ? (
+        <ProfileCard name={profileName} />
+      ) : (
+        <Pressable
+          onPress={handleProfile}
+          style={({ pressed }) => [
+            styles.editNameWrapper,
+            { opacity: pressed ? 0.6 : 1 },
+          ]}
+        >
+          <View style={styles.editNameContent}>
+            <Ionicons name="create-outline" size={18} color="#8e8e93" />
+            <Text style={styles.editNameText}>Edit your name here</Text>
+          </View>
+        </Pressable>
+      )}
+
+      {/* Notification */}
+      <FormField style={styles.notifiWrapper} title={"NOTIFICATION"}>
+        <SwitchCard
+          key={1}
+          onToggle={setIsReminderOn}
+          isActive={isReminderOn}
+          title={"Task Reminers"}
+          leftIcon={
+            <Ionicons
+              name="notifications"
+              size={24}
+              color={activeColors.primary}
+            />
+          }
+        />
+      </FormField>
+
+      {/* THEME COLOR */}
+      <FormField title={"THEME COLOR"}>
+        <View style={styles.colorPlateWrapper}>
+          {THEME_COLOR.map(color => {
+            return (
+              <ColorPlate
+                key={color}
+                colorName={color}
+                selectedColor={themeName}
+                onSelect={changeTheme}
+              />
+            );
+          })}
+        </View>
+      </FormField>
     </View>
   );
 };
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: theme.paddingL,
+    backgroundColor: "#fff",
+    height: "100%",
+  },
+  editNameWrapper: {
+    backgroundColor: "#f5f5f7",
+    borderRadius: 16,
+    paddingVertical: 18,
+    paddingHorizontal: 20,
+    marginVertical: 10,
+  },
+  editNameContent: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  editNameText: {
+    fontSize: 16,
+    color: "#8e8e93",
+    fontWeight: "500",
+    marginLeft: 8,
+  },
+  header: {
+    marginBottom: theme.marginL,
+  },
+  notifiWrapper: {
+    marginTop: theme.marginM,
+  },
+  colorPlateWrapper: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 5,
+  },
+});
 export default Index;
--- app/(tabs)/settings/profile.tsx
+++ app/(tabs)/settings/profile.tsx
@@ -1,12 +1,103 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import FormField from "@/components/common/FormField";
+import MyButton from "@/components/common/MyButton";
+import SafeHeader from "@/components/common/SafeHeader";
+import { useTheme } from "@/context/ThemeContext";
+import { useUser } from "@/context/UserContext";
+import { theme } from "@/styles/theme";
+import { router } from "expo-router";
+import React, { useState } from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
 const Profile = () => {
+  const { activeColors } = useTheme();
+  const { profileName, updateProfileName } = useUser();
+
+  const [inputText, setInputText] = useState(profileName);
+
+  const getInitials = (name: string) => {
+    const arr = name.trim().split(/\s+/);
+    if (!arr[0]) return "";
+    const first = arr[0][0].toUpperCase();
+    const last = arr.length > 1 ? arr[arr.length - 1][0].toUpperCase() : "";
+    return `${first}${last}`;
+  };
+
+  const handleSaveName = async () => {
+    const cleanName = inputText.trim();
+    await updateProfileName(cleanName);
+    router.back();
+  };
+
   return (
-    <View>
-      <Text>Profile page</Text>
+    <View style={styles.container}>
+      <SafeHeader
+        style={styles.header}
+        title="Profile"
+        rightContent={<MyButton title="Save" onClick={handleSaveName} />}
+      />
+
+      {inputText.trim() !== "" && (
+        <View style={styles.profileCircleWrapper}>
+          <View
+            style={[
+              styles.profileCircle,
+              { backgroundColor: activeColors.primary },
+            ]}
+          >
+            <Text style={styles.text}>{getInitials(inputText)}</Text>
+          </View>
+        </View>
+      )}
+
+      <FormField title="FULL NAME">
+        <TextInput
+          value={inputText}
+          onChangeText={setInputText}
+          style={[styles.profileNameInput, styles.nameInput]}
+          autoFocus={true}
+        />
+      </FormField>
     </View>
   );
 };
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: theme.paddingL,
+    backgroundColor: "#fff",
+    height: "100%",
+  },
+  header: {
+    marginBottom: theme.marginL,
+  },
+  profileCircleWrapper: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  profileCircle: {
+    borderRadius: 50,
+    width: 100,
+    height: 100,
+    justifyContent: "center",
+    alignItems: "center",
+    // padding: theme.paddingXl,
+    // backgroundColor: theme.themeColors.teal,
+    marginRight: theme.marginM,
+  },
+  text: {
+    color: theme.fontColor.bodyTextLight,
+    fontSize: theme.fontSizeXxl,
+    fontWeight: 700,
+  },
+  nameInput: {
+    padding: theme.paddingM,
+    borderWidth: 1,
+    borderRadius: 10,
+    borderColor: "#e5e5ea",
+    backgroundColor: "#f5f5f7",
+  },
+  profileNameInput: {
+    fontSize: theme.fontSizeXl,
+    fontWeight: 600,
+  },
+});
 export default Profile;
